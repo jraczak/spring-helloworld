adaafa8f6887b2878f88f76a4d7c1e76b08de125
Justin <jraczak@gmail.com>
2017-12-09 16:00:43 -0800

    More playing with template. Add bootstrap and jquery to static resources

--- src/main/java/com/tutorialspoint/helloworld/web/TestController.java
+++ src/main/java/com/tutorialspoint/helloworld/web/TestController.java
@@ -9,8 +9,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class TestController {
 
     @RequestMapping("/test")
-    public String test(Model model, @RequestParam(value="name", required=false, defaultValue = "friend") String name) {
+    public String test(Model model,
+                       @RequestParam(value="name", required=false, defaultValue = "friend") String name,
+                       @RequestParam(value="int1", required=false, defaultValue = "1") int int1,
+                       @RequestParam(value = "int2", required = false, defaultValue = "1") int int2) {
         model.addAttribute("name", name);
+        model.addAttribute("total", int1+int2);
         return "test";
     }
 
